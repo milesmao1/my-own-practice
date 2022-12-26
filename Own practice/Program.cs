172b81ac67892c6063efa8eccff5c744ce61cdb7
Miles Mao <mrmao1@gmail.com>
2022-12-26 12:33:10 -0600

    practising

---
+++ Own practice/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine($"Hi. Enter your name below");
+var teacherName = Console.ReadLine() ;
+
+
+Console.WriteLine("Hello, {teacherName}! How are you today?");
---
+++ variables my own practice/Program.cs
@@ -0,0 +1,10 @@
+﻿// Declaring and initializing variables
+string name = "Josephus";
+int age = 2000;
+char middleInitial = 'F';
+bool isOver18 = true;
+double currentTemp = 77.8;
+decimal currentPrice = 1654321.25m;
+
+Console.WriteLine ($"My dog's name is {name} {middleInitial}. He is {age} years old. It's {isOver18} he is of age." +
+    $" He is currently testing at {currentTemp}. He is not for sale but is worth {currentPrice}");
